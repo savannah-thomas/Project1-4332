2e70e07bd3f694eb403d49148747fcb701f4f066
savannah-thomas <savannah.thomas@lusfiber.net>
2025-04-21 13:35:52 -0500

    Addded code for if $100 is exclusive, and commented method.

--- src/main/java/LibraryManagementSystem/Purchasing.java
+++ src/main/java/LibraryManagementSystem/Purchasing.java
@@ -4,8 +4,14 @@ import java.util.Random;
 
 public class Purchasing {
     private static final Random rand = new Random();
-
+    //Generates a random value between $10 and $100 for a new book
+    //returns an int from 10 to 100 inclusively
     public double generateBookCost() {
-        return 10 + rand.nextInt(91); // $10 to $100
+        //Returns the value of the book
+        return 10 + rand.nextInt(91);
+
+        //90 * random.nextDouble scales the val to be in the range 0.0 to 90.0 (exclusively, so includes 89.999)
+        //And adding 10 shifts the scale by 10
+        //return 10 + (90 * rand.nextDouble());
     }
 }
