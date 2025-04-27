88ce9022339bd6f30e7d34b0cad2a889e236ca68
savannah-thomas <savannah.thomas@lusfiber.net>
2025-04-27 17:20:24 -0500

    Cleaner comments for alternative case.

--- src/main/java/LibraryManagementSystem/Purchasing.java
+++ src/main/java/LibraryManagementSystem/Purchasing.java
@@ -8,10 +8,11 @@ public class Purchasing {
     //returns an int from 10 to 100 inclusively
     public double generateBookCost() {
         //Returns the value of the book
-        return 10 + rand.nextInt(91);
-
-        //90 * random.nextDouble scales the val to be in the range 0.0 to 90.0 (exclusively, so includes 89.999)
+        return 10 + rand.nextInt(91); //gives random int b/t 0 to 90 inclusive
         //And adding 10 shifts the scale by 10
+
+        //Alternative Case (for decimals b/t 10.0 and <100,0:
+        //90 * random.nextDouble -> scales the val to be in the range 0.0 to 90.0 (exclusively, so includes 89.999)
         //return 10 + (90 * rand.nextDouble());
     }
 }
